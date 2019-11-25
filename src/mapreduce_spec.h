2185508be5b263c6f9923c432a28c4275f0721c7
Bond Wong <bond@Bonds-MacBook-Pro.local>
2019-11-24 17:19:56 -0800

    .

--- src/mapreduce_spec.h
+++ src/mapreduce_spec.h
@@ -1,16 +1,86 @@
 #pragma once
 
 #include <string>
-
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 
 /* CS6210_TASK: Create your data structure here for storing spec from the config file */
 struct MapReduceSpec {
-
+	std::string user_id;
+	int n_workers;
+	std::vector<std::string> worker_ipaddr_ports;
+	std::vector<std::string> input_files;
+	int map_kilobytes; // shard size
+	int n_output_files; // the number of regions R
+	std::string output_dir;
 };
 
+inline bool fill_marspec(MapReduceSpec& mr_spec, const std::string& key, const std::string& val) {
+	switch(key) {
+		case "user_id":
+			mr_spec.user_id = val;
+			break;
+		case "n_workers":
+			mr_spec.n_workers = val;
+			break;
+		case "worker_ipaddr_ports":
+			std::istringstream iss(val);
+			std::string file_path;
+			while (std::getline(iss, file_path, ',')) mr_spec.worker_ipaddr_ports.push_back(addr);
+			break;
+		case "input_files":
+			std::istringstream iss(val);
+			std::string file_path;
+			while (std::getline(iss, file_path, ',')) mr_spec.input_files.push_back(addr);
+			break;
+		case "map_kilobytes":
+			mr_spec.map_kilobytes = std::stoi(val);
+			break;
+		case "n_output_files":
+			mr_spec.map_kilobytes = std::stoi(val);
+			break;
+		case "output_dir":
+			mr_spec.output_dir = val;
+			break;
+		default:
+			return false;
+	}
+
+	return true;
+}
 
 /* CS6210_TASK: Populate MapReduceSpec data structure with the specification from the config file */
 inline bool read_mr_spec_from_config_file(const std::string& config_filename, MapReduceSpec& mr_spec) {
+	std::ifstream config_file(config_filename);
+	if (!config_file.is_open()) {
+		std::cerr << "Error when opening file: " << config_filename << std::endl;
+		return false;
+	}
+
+	std::string line;
+	while (std::getline(config_file, line)) {
+		// split line
+		std::istringstream line_stream(line);
+		std::string key;
+		std::string value;
+		bool isOk = std::getline(line_stream, key, '=')
+			&& std::getline(is_line, value);
+
+
+		if (!isOk) {
+			std::cerr << "Error when parsing line: " << line << std::endl;
+			return false;
+		}
+
+		// fill spec
+		if (!fill_marspec(mr_spec, key, value)) {
+			std::cerr << "Error when setting: " << key << " " << value << std::endl;
+			return false;
+		}
+	}
+	config_file.close();
 	return true;
 }
 
