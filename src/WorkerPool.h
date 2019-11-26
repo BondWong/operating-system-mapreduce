bc28b74ec1f17692f0915f49d011b96506c69e2f
Bond Wong <bond@Bonds-MacBook-Pro.local>
2019-11-25 21:29:16 -0800

    .

--- src/WorkerPool.h
+++ src/WorkerPool.h
@@ -35,7 +35,7 @@ WorkerPool::WorkerPool(const std::vector<std::string>& worker_ipaddr_ports) {
 		std::shared_ptr<grpc::Channel> channel = grpc::CreateChannel(worker_ipaddr_port,
 			grpc::InsecureChannelCredentials());
 		std::unique_ptr<masterworker::WorkerService::Stub> stub(masterworker::WorkerService::NewStub(channel));
-		workers.insert(std::pair<std::string, std::unique_ptr<masterworker::WorkerService::Stub> >(worker_ipaddr_port, stub));
+		workers.insert(std::make_pair(worker_ipaddr_port, stub));
 		free_worker_queue.push(worker_ipaddr_port);
 	}
 }
