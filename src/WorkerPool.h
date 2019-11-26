afda0aa57c7738e9d9f7a94a52ec5c0527b26382
Bond Wong <bond@Bonds-MacBook-Pro.local>
2019-11-26 00:09:17 -0800

    .

--- src/WorkerPool.h
+++ src/WorkerPool.h
@@ -43,10 +43,9 @@ WorkerPool::WorkerPool(const std::vector<std::string>& worker_ipaddr_ports) {
 std::thread WorkerPool::executeMap(const masterworker::Shard& shard, masterworker::Result* res) {
 	std::function<void()> job = [&]() {
 		std::string worker = get_worker();
-		std::unique_ptr<masterworker::WorkerService::Stub>& stub_ = workers.at(worker);
+		std::unique_ptr<masterworker::WorkerService::Stub> stub_ = workers.at(worker);
 		grpc::ClientContext context;
-		if (stub_) std::cout << "stub_ is not null" << std::endl;
-		grpc::Status status = stub_->map(&context, shard, res);
+		grpc::Status status = stub_->Map(&context, shard, res);
 		std::cout << "making map call to worker: " << worker << std::endl;
 		if (!status.ok()) std::cerr << status.error_message() << std::endl;
 		std::cout << "done map call to worker with res: " << res->file_path() << std::endl;
@@ -62,7 +61,7 @@ std::thread WorkerPool::executeReduce(const masterworker::Region& region, master
 		std::string worker = get_worker();
 		std::unique_ptr<masterworker::WorkerService::Stub>& stub_ = workers.at(worker);
 		grpc::ClientContext context;
-		grpc::Status status = stub_->reduce(&context, region, res);
+		grpc::Status status = stub_->Reduce(&context, region, res);
 		if (!status.ok()) std::cerr << status.error_message() << std::endl;
 		release_worker(worker);
 	};
