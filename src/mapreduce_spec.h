72b43c01b7dfd599cf2ac253189d77c5edf01d29
Bond Wong <bond@Bonds-MacBook-Pro.local>
2019-11-25 22:18:03 -0800

    .

--- src/mapreduce_spec.h
+++ src/mapreduce_spec.h
@@ -82,9 +82,9 @@ inline bool validate_mr_spec(const MapReduceSpec& mr_spec) {
 	std::cout << "user_id" << mr_spec.user_id <<std::endl;
 	std::cout << "n_workers" << mr_spec.n_workers <<std::endl;
 	for (auto i = mr_spec.worker_ipaddr_ports.begin(); i != mr_spec.worker_ipaddr_ports.end(); i++) std::cout << *i << ' ';
-	std::count << std::endl;
+	std::cout << std::endl;
 	for (auto i = mr_spec.worker_ipaddr_ports.begin(); i != mr_spec.input_files.end(); i++) std::cout << *i << ' ';
-	std::count << std::endl;
+	std::cout << std::endl;
 	std::cout << "map_kilobytes" << mr_spec.map_kilobytes <<std::endl;
 	std::cout << "n_output_files" << mr_spec.n_output_files <<std::endl;
 	std::cout << "output_dir" << mr_spec.output_dir <<std::endl;
